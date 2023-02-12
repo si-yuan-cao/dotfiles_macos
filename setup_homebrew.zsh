8706e75cd272b99b202497620ee20a7778ddc647
James Cao <120765863+si-yuan-cao@users.noreply.github.com>
2023-02-12 11:26:23 +0800

    Add zshenv with exists(), and link with Dotbot

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -2,6 +2,21 @@
 
 echo "\n<<< Starting Homebrew Setup >>>\n"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if exists brew; then
+  echo "brew exists, skipping install"
+else
+  echo "brew doesn't exist, continuing with install"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# TODO: Keep an eye out for a different `--no-quarantine` solution.
+# Currently, you can't do `brew bundle --no-quarantine` as an option.
+# export HOMEBREW_CASK_OPTS="--no-quarantine --no-binaries"
+# https://github.com/Homebrew/homebrew-bundle/issues/474
+
+# HOMEBREW_CASK_OPTS is exported in `zshenv` with
+# `--no-quarantine` and `--no-binaries` options,
+# which makes them available to Homebrew for the
+# first install (before our `zshrc` is sourced)
 
 brew bundle --verbose
